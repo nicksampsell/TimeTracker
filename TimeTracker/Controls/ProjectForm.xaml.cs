148cb16a47a434c552df1afda776876b7d4816c4
nicksampsell <nick@nicksampsell.com>
2022-10-24 18:35:40 -0400

    Add fields  to ProjectForm component.

---
+++ TimeTracker/Controls/ProjectForm.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TimeTracker.Controls
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class ProjectForm : UserControl
+    {
+        public string Title
+        {
+            get { return frmTitle.Text; }
+            set { frmTitle.Text = value; }
+        }
+
+        public string Department
+        {
+            get { return frmDepartment.Text; }
+            set { frmDepartment.Text = value; }
+        }
+
+        public string Description
+        {
+            get { return frmDescription.Text; }
+            set { frmDescription.Text = value; }
+        }
+
+        public string AllocatedHours
+        {
+            get { return frmAllocatedHours.Text; }
+            set { frmAllocatedHours.Text = value; }
+        }
+
+        public Boolean IsCompleted
+        {
+            get { return (bool)frmIsCompleted.IsChecked; }
+
+            set { frmIsCompleted.IsChecked = value; }
+        }
+
+        public ProjectForm()
+        {
+            InitializeComponent();
+           
+        }
+    }
+}
