732bc9d73a5a1b96f7f149eb3429ea2b125a4485
nicksampsell <nick@nicksampsell.com>
2022-10-29 16:59:44 -0400

    More work on form validation.

---
+++ TimeTracker/Validation/TextBoxValidation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace TimeTracker.Validation
+{
+    public class TextBoxValidation : ValidationRule
+    {
+
+        public Boolean AllowLetters { get; set; } = true;
+        public Boolean AllowNumbers { get; set; } = true;
+        public Boolean AllowSpecialChars { get; set; } = true;
+        public Boolean AllowSpaces { get; set; } = true;
+        public Boolean AllowLineBreaks { get; set; } = true;
+        public int? MaxLength { get; set; } 
+
+        public TextBoxValidation()
+        {
+
+        }
+
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if (!AllowNumbers && ((string)value).Any(ch => Char.IsNumber(ch)))
+            {
+                return new ValidationResult(false, "This field cannot contains numbers.");
+            }
+
+            return ValidationResult.ValidResult;
+        }
+    }
+}
